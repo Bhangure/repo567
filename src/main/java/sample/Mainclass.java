554e0ac203704f9e7814eab6d757d4915f181bbc
Bhangure <112766432+Bhangure@users.noreply.github.com>
2023-02-09 08:38:06 +0530

    updated java file

--- src/main/java/sample/Mainclass.java
+++ src/main/java/sample/Mainclass.java
@@ -4,6 +4,6 @@ public class Mainclass {
 
     public static void main(String[] args) {
         System.out.println("this is hello world");
-          System.out.println("this is hello world");
+          System.out.println("i am asha");
     }
 }
